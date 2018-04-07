37269eec556b03156c7b3020aa8e29080e6ed075
José Addario <j.addario@gmail.com>
2018-04-07 19:17:16 -0300

    Estudo sobre herança

--- models.py
+++ models.py
@@ -17,6 +17,14 @@ class Perfil(object):
 	def obter_curtidas(self):
 		return self.__curtidas	 		
 
+class Perfil_Vip(Perfil):
+	'Classe padrão de Perfil Vip'
+	def __init__ (self, nome, telefone, empresa, apelido):
+		super(Perfil_Vip, self).__init__(nome, telefone, empresa)
+		self.apelido = apelido
+
+	def obter_creditos(self):
+		return super(Perfil_Vip, self).obter_curtidas() * 10.0	
 
 class Data(object):
 	"Classe data com metodo de formatação"
